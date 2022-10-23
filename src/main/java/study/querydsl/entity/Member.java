2d2a90197f82463cb02bfe3020ffe9e77313a2f1
kjh <poiuy5947@gmail.com>
2022-10-23 22:35:20 +0900

    Project Init

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,49 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"}) // toString 에 team을 넣으면 왔다갔다 하면서 무한루프 가능성..
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id") //외래키명으로 잡힘
+    private Team team;
+
+    public Member(String username) {
+        this(username, 0, null);
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+
+
+
+
+}
---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,30 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA는 기본생성자가 필요..
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team") // mappedBy == 연관관계 주인이 아니다..
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+}
